d2db32820fd38f65445e79b58333af139a4e455e
rootreboot <58434887+RootReboot@users.noreply.github.com>
2020-02-19 15:48:52 +0000

    Call the database initializer

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,5 +1,11 @@
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./schema");
+const { createStore } = require("./utils");
+
+const LaunchAPI = require("./schema");
+const UserAPI = require("./datasources/user");
+
+const store = createStore();
 
 const server = new ApolloServer({ typeDefs });
 
