35e16f0d2f2a939ea3720635dc89204c7dc7dee8
rootreboot <58434887+RootReboot@users.noreply.github.com>
2020-02-22 20:59:18 +0000

    Fix resolver typing error

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -30,37 +30,36 @@ module.exports = {
     bookTrips: async (_, { launchIds }, { dataSources }) => {
       const results = await dataSources.userAPI.bookTrips({ launchIds });
       const launches = await dataSources.launchAPI.getLaunchesByIds({
-        launchIds,
+        launchIds
       });
-  
+
       return {
         success: results && results.length === launchIds.length,
         message:
           results.length === launchIds.length
-            ? 'trips booked successfully'
+            ? "trips booked successfully"
             : `the following launches couldn't be booked: ${launchIds.filter(
-                id => !results.includes(id),
+                id => !results.includes(id)
               )}`,
-        launches,
+        launches
       };
     },
     cancelTrip: async (_, { launchId }, { dataSources }) => {
       const result = await dataSources.userAPI.cancelTrip({ launchId });
-  
+
       if (!result)
         return {
           success: false,
-          message: 'failed to cancel trip',
+          message: "failed to cancel trip"
         };
-  
+
       const launch = await dataSources.launchAPI.getLaunchById({ launchId });
       return {
         success: true,
-        message: 'trip cancelled',
-        launches: [launch],
+        message: "trip cancelled",
+        launches: [launch]
       };
     },
-  },
     login: async (_, { email }, { dataSources }) => {
       const user = await dataSources.userAPI.findOrCreateUser({ email });
       if (user) return Buffer.from(email).toString("base64");
